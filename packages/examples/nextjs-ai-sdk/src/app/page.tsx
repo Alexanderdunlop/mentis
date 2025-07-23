09a5246c7ee790067801f3f6d16eb429ee0a4320
Alexanderdunlop <alexjamesdunlop@gmail.com>
2025-07-23 16:33:01 +0100

    docs: add Next.js AI SDK example

---
+++ packages/examples/nextjs-ai-sdk/src/app/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useChat } from "@ai-sdk/react";
+import { MentionInput } from "mentis";
+import "mentis/dist/index.css";
+
+export default function Chat() {
+  const { messages, input, setInput, append } = useChat();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    append({ role: "user", content: input });
+  };
+
+  return (
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.map((message) => (
+        <div key={message.id} className="whitespace-pre-wrap">
+          {message.role === "user" ? "User: " : "AI: "}
+          {message.parts.map((part, i) => {
+            switch (part.type) {
+              case "text":
+                return <div key={`${message.id}-${i}`}>{part.text}</div>;
+            }
+          })}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <MentionInput
+          slotsProps={{
+            container: {
+              className: "fixed bottom-0 w-full mb-8 max-w-md",
+            },
+          }}
+          options={[
+            {
+              label: "John Doe",
+              value: "john-doe",
+            },
+          ]}
+          // defaultValue={input}
+          onChange={(e) => setInput(e.rawText)}
+          // placeholder="Say something..."
+        />
+      </form>
+    </div>
+  );
+}
