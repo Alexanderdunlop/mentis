394b6f6b4d982b926c8331fc2fadc2c7c4778a91
Alexanderdunlop <alexjamesdunlop@gmail.com>
2025-07-23 22:53:15 +0100

    docs: update mentis to version 0.2.0-alpha.2 in package.json and pnpm-lock.yaml; refactor MentionInput usage in page.tsx

--- packages/examples/nextjs/src/app/page.tsx
+++ packages/examples/nextjs/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { MentionInput, type MentionOption } from "mentis";
 import "mentis/dist/index.css";
 
@@ -19,10 +20,15 @@ const options: MentionOption[] = [
 ];
 
 export default function Home() {
+  const [value, setValue] = useState("");
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <MentionInput options={options} />
+        <MentionInput
+          options={options}
+          value={value}
+          onChange={(value) => setValue(value.value)}
+        />
       </main>
     </div>
   );
